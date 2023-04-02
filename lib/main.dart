8f267c4842415ee2ebea5ceb0717fc4512374303
Yash Sutar <yashsutar777@gmail.com>
2023-04-02 12:35:06 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:chat_app/helper/helper_function.dart';
+import 'package:chat_app/pages/auth/login_page.dart';
+import 'package:chat_app/pages/homepage.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _isSignedIn = false;
+  @override
+  void initState() {
+    super.initState();
+    getUserLoggedInStatus();
+  }
+
+  void getUserLoggedInStatus() async {
+    await HelperFunctions.getUserLoggedInStatus().then((value) {
+      if (value != null) {
+        setState(() {
+          _isSignedIn = value;
+        });
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: _isSignedIn ? HomePage() : LoginPage(),
+    );
+  }
+}
---
+++ lib/shared/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:361544286895:android:47c3f318ad3d63c52fca5a";
+  static String apiKey = "AIzaSyDPax_ro5eOKx86osfLA5c27DK0VF7d5Fc";
+  static String projectId = "chap-app-flutter";
+}
